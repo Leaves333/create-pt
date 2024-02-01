a7f3b8215a1301ffa392f85acce46a19da2e7352
Leaves <34007677+Leaves333@users.noreply.github.com>
2024-02-01 22:11:27 +0000

    real progwess

--- main.py
+++ main.py
@@ -8,24 +8,38 @@ def do_image(image, resolution):
 
 image_path = input("enter path to image: ")
 
-try:
-    with Image.open(image_path, "r") as im:
+with Image.open(image_path, "r") as im:
 
-        print(im.size, im.format, im.mode)
+    print(im.size, im.format, im.mode)
 
-        # convert image to grayscale
-        im = im.convert("L")
-        outfile = image_path[:image_path.rfind(".")] + "_grayscale.png"
-        im.save(outfile, "PNG")
+    # convert image to grayscale
+    im = im.convert("L")
+    outfile = image_path[:image_path.rfind(".")] + "_grayscale.png"
+    im.save(outfile, "PNG")
 
-        # resize image to cleanly have 3/5 ratio
-        # ImageOps.fit(im, size).save("imageops_fit.png")
-        xsize = (im.size[0] // 3) * 3
-        ysize = (im.size[1] // 5) * 5
-        im = ImageOps.fit(im, (xsize, ysize))
-        outfile = image_path[:image_path.rfind(".")] + "_resized.png"
-        im.save(outfile, "PNG")        
+    # resize image
+    RATIO = 1
+    xsize = int((im.size[0] / 3) * RATIO)
+    ysize = int((im.size[1] / 7) * RATIO)
+    im = im.resize((xsize, ysize))
 
-except Exception as err:
-    print(err)
-    exit()
+    outfile = image_path[:image_path.rfind(".")] + "_resized.png"
+    im.save(outfile, "PNG")
+
+    # convert image to text
+    # 255 = white, 0 = black
+
+    # colors from https://paulbourke.net/dataformats/asciiart/
+    chars = " .:-=+*#%@"
+    output = []
+    for y in range(im.size[1]):
+        cur_line = ""
+        for x in range(im.size[0]):
+            index = int(im.getpixel((x, y)) / 255 * len(chars))
+            index = min(index, len(chars) - 1)
+            cur_line += chars[index]
+        output.append(cur_line)
+    
+    with open("out.txt", "w") as file:
+        for line in output:
+            file.write(line + "\n")
